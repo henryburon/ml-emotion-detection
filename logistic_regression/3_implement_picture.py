cc568dc93f25d87cde5bd125408f11e4bb004134
Henry Buron <henryburon34@gmail.com>
2023-12-02 13:26:44 -0600

    Adding picture and video implementation of model. Thought they were committed in previous commit?

---
+++ logistic_regression/3_implement_picture.py
@@ -0,0 +1,156 @@
+
+from PIL import Image, ImageDraw, ImageFont
+import os
+import numpy as np
+import time
+from skimage.feature import hog
+np.set_printoptions(threshold=np.inf)  # Can use this to make it print out entire array
+import joblib
+
+##########################################################################################
+
+# The purpose of this file is to actually implement the trained Linear Regression model on images and classify them
+# It works by loading in the saved trained linear regression model
+# It then loads in the images we want to classify, and converts them to 48x48 grayscale images
+# Then, it extracts the features, and with those features, runs the loaded Linear Regression model
+# Finally, it writes the classified emotion on the image and saves it into a folder
+
+##########################################################################################
+
+start_time = time.time()
+
+# Currently is loading in a model trained with 7 classes and max 3k training images per class
+loaded_model = joblib.load('logistic_regression/trained_model/2class_3ksamples_77%.pk1')
+
+emotion_indices = {
+    0: 'angry',
+    1: 'happy',
+    2: 'neutral',
+    3: 'sad',
+    4: 'disgusted',
+    5: 'fearful',
+    6: 'surprised'
+}
+
+#####################################################################################################
+# Load in images, reshape to 48x48 pixels, and make grayscale
+#####################################################################################################
+
+folder_path = 'images/unknown/original'  # Replace this with the path to your folder containing images
+
+image_list = []
+saved_image_list = []
+
+# Iterate through all images in the folder
+for filename in os.listdir(folder_path):
+    file_path = os.path.join(folder_path, filename)
+    
+    if os.path.isfile(file_path) and filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
+        try:
+            with Image.open(file_path) as img:
+                # Process the image here (e.g., display or perform operations)
+                # img.show()  # Opens the image using the default viewer
+                img.load()
+                saved_image_list.append(img)
+                img = img.resize((48, 48)).convert('L')
+                image_list.append(img)
+        except Exception as e:
+            print(f"Could not open {filename}: {e}")
+
+#####################################################################################################
+# Convert to 48x48 numerical arrays, normalize, extract hog features
+#####################################################################################################
+
+image_array_list = []
+
+for image in image_list:
+    image = np.array(image)/255
+    image_array_list.append(image)
+
+image_features_list = []
+image_hog_list = []
+
+num_orientation = 12
+num_cells = 8
+num_block = 2
+
+for image_array in image_array_list:
+    features, hog_image = hog(
+        image_array,
+        orientations=num_orientation,
+        pixels_per_cell=(num_cells, num_cells),
+        cells_per_block=(num_block, num_block),
+        visualize=True,
+    )
+    image_features_list.append(features)
+    image_hog_list.append(hog_image)
+
+#####################################################################################################
+# Run Linear Regression
+#####################################################################################################
+
+# Numerical Predictions
+predictions = loaded_model.predict(image_features_list)
+print(predictions)
+
+# Convert numerical predictions to corresponding emotion
+predicted_emotions = [emotion_indices[prediction] for prediction in predictions]
+print(predicted_emotions)
+
+#####################################################################################################
+# Write emotion on image
+#####################################################################################################
+
+def add_text_to_image(image, text_to_add, output_path):
+    copied_image = image.copy()
+
+    draw = ImageDraw.Draw(copied_image)
+
+    font = ImageFont.truetype('FreeMono.ttf', 35)
+
+    text_width = len(text_to_add) * 20
+    text_height = 20
+
+    image_width, image_height = copied_image.size
+    text_position = ((image_width - text_width) // 2, image_height - text_height - 10)  # Offset by 10 pixels from the bottom
+
+    # Define background rectangle coordinates
+    background_position = (
+        text_position[0] - 5,  # Adjust the x-coordinate to set the background width
+        text_position[1] + 5,  # Adjust the y-coordinate to set the background height
+        text_position[0] + text_width + 5,  # Adjust the width of the background rectangle
+        text_position[1] + text_height + 15  # Adjust the height of the background rectangle
+    )
+
+    # Draw a black rectangle as background
+    draw.rectangle(background_position, fill="black")
+
+    draw.text(text_position, text_to_add, fill="white", font=font)
+
+    copied_image.save(output_path)
+
+output_folder = "images/unknown/classified"
+
+# Ensure the output folder exists, create it if necessary
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Iterate through images in image_list and predicted_emotions
+for index, (img, emotion) in enumerate(zip(saved_image_list, predicted_emotions)):
+    output_path = f"{output_folder}/image_{index + 1}_emotion_{emotion}.jpg"  # Replace with desired output format
+
+    # Add text (predicted emotion) to the image and save
+    add_text_to_image(img, emotion, output_path)
+
+
+
+
+
+
+#####################################################################################################
+# Time calculation
+#####################################################################################################
+
+end_time = time.time()
+total_time = end_time - start_time
+print(f"Total time {total_time} seconds")
---
+++ logistic_regression/4_implement_video.py
@@ -0,0 +1,66 @@
+import cv2
+from skimage.feature import hog
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import joblib
+
+# Load your trained logistic regression model
+loaded_model = joblib.load('logistic_regression/trained_model/2class_3ksamples_77%.pk1')
+
+# Mapping indices to emotions
+emotion_indices = {
+    0: 'angry',
+    1: 'happy',
+    2: 'neutral',
+    3: 'sad',
+    4: 'disgusted',
+    5: 'fearful',
+    6: 'surprised'
+}
+
+# Function to detect emotion in a frame and overlay it on the screen
+def detect_emotion(frame):
+    # Convert the frame to grayscale and resize to 48x48
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_resized = cv2.resize(gray, (48, 48))
+
+    # Extract HOG features
+    features = hog(
+        gray_resized,
+        orientations=12,
+        pixels_per_cell=(8, 8),
+        cells_per_block=(2, 2),
+        visualize=False
+    )
+
+    # Reshape the features to match the model input shape
+    features = features.reshape(1, -1)
+
+    # Predict emotion using the loaded model
+    prediction = loaded_model.predict(features)[0]
+
+    # Map the predicted index to emotion label
+    predicted_emotion = emotion_indices[prediction]
+
+    # Overlay the predicted emotion on the frame
+    cv2.putText(frame, predicted_emotion, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+
+    return frame
+
+# Start capturing video from the webcam
+cap = cv2.VideoCapture(0)  # Use '0' for the default webcam
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the frame with detected emotion
+    cv2.imshow('Emotion Detection', detect_emotion(frame))
+
+    # Press 'q' to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture and destroy any OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
